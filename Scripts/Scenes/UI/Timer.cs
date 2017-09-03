0cef3de490e0edaf36b2b6220888b9cd0c3bb533
pedroolivagil <onion_oliva@hotmail.com>
2017-09-04 01:18:13 +0200

    CountDown v1

--- Scripts/Scenes/UI/Timer.cs
+++ Scripts/Scenes/UI/Timer.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour{
+    public int timeSeconds;
+    public bool countDown;
+
     private Text timer;
     private int time;
-    int mins = 0;
-    int seconds = 0;
+    float mins = 0;
+    float secs = 0;
 
     void Start(){
-        time = 0;
         timer = GetComponentInChildren<Text>();
+        if (countDown){
+            InitCountDown();
+        }
+        else{
+            InitCounter();
+        }
+    }
+
+    public void InitCountDown(){
+        time = timeSeconds;
+        timer.text = "00:00";
+        StartCoroutine(UpdateTimerCountDown());
+    }
+
+    private void InitCounter(){
+        time = timeSeconds;
         timer.text = "00:00";
         StartCoroutine(UpdateTimer());
     }
@@ -20,32 +38,66 @@ public class Timer : MonoBehaviour{
     private IEnumerator UpdateTimer(){
         yield return new WaitForSeconds(1f);
         timer.text = CalcTimeText();
-        yield return UpdateTimer();
+        if (time < 6039){
+            yield return UpdateTimer();
+        }
+    }
+
+    private IEnumerator UpdateTimerCountDown(){
+        yield return new WaitForSeconds(1f);
+        timer.text = CalcTimeTextCountDown();
+        if (time > 0){
+            yield return UpdateTimerCountDown();
+        }
     }
 
     private string CalcTimeText(){
-        time++;
-        if (time % 60 == 0){
-            mins++;
-            seconds = 0;
-        }
-        else{
-            seconds++;
-        }
         String text = "";
         String minsText = "";
         String secsText = "";
+        time++;
+        Debug.Log("Time: " + time);
+        if (time % 60 == 0){
+            mins++;
+            secs = 0;
+        }
+        else{
+            secs++;
+        }
         if (mins < 10){
             minsText = "0" + mins;
         }
         else{
             minsText = "" + mins;
         }
-        if (seconds < 10){
-            secsText = "0" + seconds;
+        if (secs < 10){
+            secsText = "0" + secs;
         }
         else{
-            secsText = "" + seconds;
+            secsText = "" + secs;
+        }
+        text = minsText + ":" + secsText;
+        return text;
+    }
+
+    private string CalcTimeTextCountDown(){
+        String text = "";
+        String minsText = "";
+        String secsText = "";
+        time--;
+        mins = time / 60;
+        secs = ((time / 60f) - mins) * 60;
+        if (mins < 10){
+            minsText = "0" + mins;
+        }
+        else{
+            minsText = "" + mins;
+        }
+        if (secs < 10){
+            secsText = "0" + secs;
+        }
+        else{
+            secsText = "" + secs;
         }
         text = minsText + ":" + secsText;
         return text;
